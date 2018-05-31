8c383498b01331fcc0915d6ba02f31c377d0039c
Diana Shin <ayoung0131@gmail.com>
2018-05-30 21:28:12 -0400

    delete redundant files

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AntRace from './components/antRace';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<AntRace />, document.getElementById('root'));
-registerServiceWorker();
